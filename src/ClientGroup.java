302346ae4dc4cb9ec6247fda12f69f809cc1361d
OniongRu <light.smoky@yandex.ru>
2020-10-25 14:55:34 +0300

    Comments

--- src/ClientGroup.java
+++ src/ClientGroup.java
@@ -9,9 +9,13 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
+//класс объекта, работающего в отдельном потоке, взаимодействующий с Сокетами,
+//записанными в его селектор
 public class ClientGroup extends Thread{
     public Selector SelectorS;
     int PORT;
+
+    //конструктор
     public ClientGroup(SocketChannel ServElSoc, int PORT) throws IOException {
         SelectorS = Selector.open();
         ServElSoc.configureBlocking(false);
@@ -20,26 +24,26 @@ public class ClientGroup extends Thread{
         this.PORT = PORT;
         start();
     }
+
+    //добавление нового сокета в селектор
     public void AddSocket(SocketChannel ClientS) throws IOException {
         ClientS.configureBlocking(false);
         ClientS.register(SelectorS, SelectionKey.OP_READ);
         System.out.println(ClientS.getRemoteAddress()+" ♫CONNECTED♫");
     }
+
     public void run()
     {
         try {
             while (true) {
                 int res = 0;
-                res = SelectorS.select(100);
+                res = SelectorS.select(100); //ожидание действий от клиентов
                 if (res > 0) {
-                    Set<SelectionKey> selectedKeys = SelectorS.selectedKeys();
+                    Set<SelectionKey> selectedKeys = SelectorS.selectedKeys(); //создание ключей для соединений, от которых пришли запросы
                     Iterator<SelectionKey> iter = selectedKeys.iterator();
                     while (iter.hasNext()) {
                         SelectionKey key = iter.next();
                         if (key.isReadable()) {
-                            //showClientAnswer(key);
-                            //RequestHandler ReadReq = new RequestHandler(key,0);
-
                             takeGson(key);
                         }
                         iter.remove();
@@ -51,6 +55,8 @@ public class ClientGroup extends Thread{
             System.out.println("♂PogCham Server is down♂");
         }
     }
+
+    //приём json-а от клиента и преобразование в объекта DataPack
     private static void takeGson(SelectionKey key)
     {
         try {
@@ -82,47 +88,5 @@ public class ClientGroup extends Thread{
             }
         }
     }
-/*
-    private static void showClientAnswer(SelectionKey key) {
-        try {
-        SocketChannel client = (SocketChannel) key.channel();
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        client.read(buffer);
-        String text = new String(buffer.array(),"UTF-8").trim();
-        if(text.length()>0) {
-            System.out.println(currentThread().getId() + client.getRemoteAddress().toString() + " | data: |" + text + "|");
-        }
-        String end_msg = new String("EndThisConnection".getBytes(),"UTF-8").trim();
-
-        if(text.equals(end_msg)) {
-            try {
-                System.out.println(((SocketChannel) key.channel()).getRemoteAddress() + " #DISCONNECTED_GOOD# from thread" + currentThread().getId() + " ");
-                key.channel().close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-            if(text.equals("ClientCppMsg#4"))
-            {
-                String msg = "t"+currentThread().getId()+"_end"+(SocketChannel)key.channel()+"#";
-                ByteBuffer buffCpp = ByteBuffer.allocate(1024);
-                buffCpp.put(msg.getBytes());
-                buffCpp.flip();
-                client.write(buffCpp);
-                try {
-                    System.out.println(((SocketChannel) key.channel()).getRemoteAddress() + " #DISCONNECTED_GOOD# from thread" + currentThread().getId() + " ");
-                    key.channel().close();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-            }
-    }catch (IOException e) {
-            try {
-                System.out.println(((SocketChannel)key.channel()).getRemoteAddress()+" #DISCONNECTED# from thread"+currentThread().getId()+" ");
-                key.channel().close();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-    }*/
+    
 }
--- src/Server.java
+++ src/Server.java
@@ -5,7 +5,8 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
 
-
+//класс сервер принимает входящие соединения и назначает
+//их на селектор в объекты класса ClientGroup (1 объект = 1 поток)
 public class Server {
     private static final int PORT = 5020;
     public static ArrayList<ClientGroup> serverList = new ArrayList<>();
@@ -14,16 +15,16 @@ public class Server {
             System.out.println("♂Server start♂");
             ServerSocketChannel ServerS = ServerSocketChannel.open();
             ServerS.bind(new InetSocketAddress(PORT));
-            SocketChannel ClientS1 = ServerS.accept();
-            serverList.add(new ClientGroup(ClientS1,PORT));
-            ClientS1 = ServerS.accept();
-            serverList.add(new ClientGroup(ClientS1,PORT));
+            SocketChannel ClientS1 = ServerS.accept(); // ожидание входящего соединения
+            serverList.add(new ClientGroup(ClientS1,PORT)); //инициализаия 1ого объекта 
+            ClientS1 = ServerS.accept(); // ожидание входящего соединения 
+            serverList.add(new ClientGroup(ClientS1,PORT)); //инициализаия 2ого объекта 
             int ThreadVar=0;
             while(true)
             {
                 SocketChannel ClientS = ServerS.accept();
                 ClientS.configureBlocking(false);
-                serverList.get(ThreadVar).AddSocket(ClientS);
+                serverList.get(ThreadVar).AddSocket(ClientS); // регистрация входящего соединения в селекторе одного из объекта 
                 ThreadVar = (ThreadVar + 1) % 2;
             }
 
