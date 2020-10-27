ad30d509e24adc24569b1d5e8eeaeec27bc49dc9
Иван Иванов <bolotin99@inbox.ru>
2020-10-27 19:17:32 +0300

    Delete excess import, 'date' and 'resource' make private

--- src/databaseInteract/HourInf.java
+++ src/databaseInteract/HourInf.java
@@ -1,16 +1,14 @@
 package databaseInteract;
 
-import databaseInteract.ResourceUsage;
-
 import java.util.Date;
 
 //Only getters here because there is no point in changing pack info https://vk.com/sticker/1-163-64
 public class HourInf {
     //DateFormat outputformat = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
-    Date date;
+    private Date date;
     private int timeSum;
     private int timeActSum;
-    ResourceUsage resource;
+    private ResourceUsage resource;
 
     public Date getDate() {
         return date;
