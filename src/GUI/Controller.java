1e2559575a872c4b0f0c7c760a9f4475b615ba3e
rururu387 <rururu387@gmail.com>
2020-11-09 00:00:46 +0300

    Improved stability. Error printing is pretty now)

---
+++ src/GUI/Controller.java
@@ -0,0 +1,282 @@
+package GUI;
+
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import threadManager.ThreadController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.io.File;
+import java.io.IOException;
+
+public class Controller {
+    @FXML
+    private AnchorPane pane;
+
+    @FXML
+    private HBox titleBar;
+
+    @FXML
+    private ImageView minimizeButton;
+
+    @FXML
+    private ImageView closeButton;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Text statusText;
+
+    @FXML
+    private ImageView toggleButton;
+
+    @FXML
+    private Text errorMessage;
+
+    private Stage window;
+
+    private final Mouse mouse = new Mouse();
+
+    private boolean isTrayIconExist = false;
+
+    private static final String stylePath = "GUI/style/";
+
+    public static final int PORT = 5020;
+
+    ThreadController thController = null;
+
+    Thread connectionAcceptTh = new Thread();
+
+    public Controller() {
+        thController = new ThreadController();
+    }
+
+    @FXML
+    private void onCloseReleased(MouseEvent event) {
+        window = (Stage) (closeButton).getScene().getWindow();
+        if (thController.getIsServerToggledOff())
+        {
+            closeApp();
+        }
+        else
+        {
+            window.hide();
+            if (!isTrayIconExist) {
+                javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+                isTrayIconExist = true;
+            }
+        }
+    }
+
+    @FXML
+    private void onMinimizeReleased(MouseEvent event) {
+        ((Stage) (minimizeButton).getScene().getWindow()).setIconified(true);
+    }
+
+    @FXML
+    private void onToggleSwitch(MouseEvent event) {
+        toggleSwitch();
+    }
+
+    //Modified source https://gist.github.com/jonyfs/b279b5e052c3b6893a092fed79aa7fbe#file-javafxtrayiconsample-java-L86
+    private void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            java.awt.Toolkit.getDefaultToolkit();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                closeApp();
+            }
+
+            // set up a system tray icon.
+            java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+            File imageFile = new File("src/" + stylePath + "trayIconSmallAngry.png");
+            java.awt.Image image = ImageIO.read(imageFile);
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(image);
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // if the user selects the default menu item (which includes the app name),
+            // show the main app stage.
+            java.awt.MenuItem openItem = new java.awt.MenuItem("Pop up");
+            openItem.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // toggle logging via tray, add item to popup menu, change it's displayed label
+            java.awt.MenuItem toggleItem = new java.awt.MenuItem("Turn off");
+            toggleItem.addActionListener(event -> { toggleSwitch(); });
+
+            //Tray icon listener. Used to show tray label correctly - toggle off when switched on and vise versa
+            trayIcon.addMouseMotionListener(new MouseAdapter() {
+                @Override
+                public void mouseMoved(java.awt.event.MouseEvent e) {
+                    if (thController.getIsServerToggledOff())
+                    {
+                        toggleItem.setLabel("Turn on");
+                    }
+                    else
+                    {
+                        toggleItem.setLabel("Turn off");
+                    }
+                }
+            });
+
+            // the convention for tray icons seems to be to set the default icon for opening
+            // the application stage in a bold font.
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            openItem.setFont(boldFont);
+
+            // to really exit the application, the user must go to the system tray icon
+            // and select the exit option, this will shutdown JavaFX and remove the
+            // tray icon (removing the tray icon will also shut down AWT).
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                tray.remove(trayIcon);
+                closeApp();
+            });
+
+            // setup the popup menu for the application.
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(toggleItem);
+            popup.add(openItem);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (AWTException | IOException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+    private void showStage() {
+        if (window != null) {
+            window.show();
+            window.toFront();
+        }
+    }
+
+    public void closeApp() {
+        thController.sendClose();
+        /*try {
+            thController.closeService();
+        }catch (IOException e) {
+            errorMessage.setText("Could not close connection correctly");
+            errorMessage.setVisible(true);
+        }*/
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void onTurnedOn(){
+        statusText.setFill(Paint.valueOf("#9de05c"));
+        statusText.setText("Turn off");
+        toggleButton.setImage(new Image(stylePath + "turnOnButtonSmall.png"));
+        nameField.setDisable(true);
+        passwordField.setDisable(true);
+    }
+
+    public void onTurnedOff(){
+        statusText.setFill(Paint.valueOf("#f8902f"));
+        statusText.setText("Turn on");
+        toggleButton.setImage(new Image(stylePath + "turnOffButtonSmall.png"));
+        nameField.setDisable(false);
+        passwordField.setDisable(false);
+    }
+
+    private void toggleSwitch()
+    {
+        if (thController.getIsServerToggledOff()) {
+            onTurnedOn();
+            Thread.UncaughtExceptionHandler h = (th, ex) -> {
+                //try {
+                thController.sendClose();
+                /*}catch(IOException e) {
+                    errorMessage.setText("Error in receiving data. Could not close connection correctly");
+                    errorMessage.setVisible(true);
+                }*/
+                errorMessage.setText("Can't launch server: no internet connection");
+                errorMessage.setVisible(true);
+                onTurnedOff();
+            };
+            connectionAcceptTh = new Thread() {
+                @Override
+                public void run() {
+                    try{
+                    thController.launchService(PORT);
+                    } catch (PrettyException e) {
+                        errorMessage.setText(e.getPrettyMessage());
+                        errorMessage.setVisible(true);
+                        try {
+                            thController.closeService();
+                        } catch (IOException ioException) {
+                            errorMessage.setText(e.getPrettyMessage() + "\n" + "Also could not close connection correctly");
+                            errorMessage.setVisible(true);
+                        }
+                        //System.out.println(e.toString());
+                        //I hate this thing =(
+                        //Was stuck here for hours
+                        throw new RuntimeException();
+                    }
+                }
+            };
+            connectionAcceptTh.setUncaughtExceptionHandler(h);
+            connectionAcceptTh.start();
+        }
+        else {
+            onTurnedOff();
+            thController.sendClose();
+            /*try{
+                thController.closeService();
+            }catch (IOException e) {
+                errorMessage.setText("Could not close connection correctly");
+                errorMessage.setVisible(true);
+            }*/
+        }
+    }
+
+    public void initialize()
+    {
+        titleBar.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent t) {
+                mouse.setX(t.getX());
+                mouse.setY(t.getY());
+            }
+        });
+        titleBar.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                titleBar.getScene().getWindow().setX(t.getScreenX() - mouse.getX());
+                titleBar.getScene().getWindow().setY(t.getScreenY() - mouse.getY());
+            }
+        });
+        pane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                errorMessage.setVisible(false);
+            }
+        });
+    }
+}
---
+++ src/GUI/Main.java
@@ -0,0 +1,46 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application {
+
+    public static Stage window;
+    private static Controller controller;
+    private static final String stylePath = "GUI/style/";
+
+    @Override public void start(Stage primaryStage) throws Exception {
+        Platform.setImplicitExit(false);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        window = primaryStage;
+        window.getIcons().add(new Image(stylePath + "knifeGoose.png"));
+        window.initStyle(StageStyle.TRANSPARENT);
+
+        Scene windowScene = new Scene(root, 400, 230);
+        window.setResizable(false);
+        window.setScene(windowScene);
+
+        window.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                controller.closeApp();
+            }
+        });
+
+        controller = new Controller();
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        controller.initialize();
+    }
+}
