0aa383b08782a7bbd32df5fb9751d441ef2c5dab
Иван Иванов <bolotin99@inbox.ru>
2020-11-10 23:48:38 +0300

    Remake add methods TO DO: add Date in DB

--- src/DBManager/DBManager.java
+++ src/DBManager/DBManager.java
@@ -1,6 +1,5 @@
 package DBManager;
 
-
 import databaseInteract.Program;
 import databaseInteract.ResourceUsage;
 import databaseInteract.User;
@@ -9,11 +8,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
-
 
 public class DBManager {
 
@@ -33,29 +34,31 @@ public class DBManager {
     public void addUser(User user) throws SQLException {
         Statement statement = conn.createStatement();
         int rows = statement.executeUpdate(String.format(
-                "INSERT users(user_name,email,login,password) VALUES (%s, %s, %s)"
-                , user.getName(), user.getLogin(), user.getPassword()));
-        System.out.printf("Added %d rows", rows);
+                "INSERT users(user_id, user_name, login, password) VALUES (%d, '%s', '%s', '%s')"
+                , user.getID(), user.getName(), user.getLogin(), user.getPassword()));
+        System.out.printf("Added %d rows at table users\n", rows);
     }
 
     public void addProgram(Program program, int id) throws SQLException {
         Statement statement = conn.createStatement();
         int rows = statement.executeUpdate(String.format(
-                "INSERT programm (program_name, user_id) VALUES (%s, %d)"
+                "INSERT program (program_name, user_id) VALUES ('%s', %d)"
                 , program.getName(), id));
-        System.out.printf("Added %d rows", rows);
+        System.out.printf("Added %d rows at table program\n", rows);
     }
 
-    public void addResourceUsage(ResourceUsage resource,int id) throws SQLException {
+    public void addResourceUsage(ResourceUsage resource, int id) throws SQLException {
         Statement statement = conn.createStatement();
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         int rows = statement.executeUpdate(String.format(
-                "INSERT ResourceUsage (date_using, cpu_avg,ram_avg,program_id) VALUES (%t, %f, %f, %d)"
-                , new Date(), resource.get_cpuUsage(), resource.get_ramUsage(), id));
-        System.out.printf("Added %d rows", rows);
+                "INSERT resourceUsage (date_using, cpuUsage, ramUsage, program_id, threadAmount) VALUES (%f, %d, %d, %d)"
+                , resource.get_cpuUsage(), resource.get_ramUsage(), id, resource.get_threadAmount()));
+        System.out.printf("Added %d rows at table resourceUsage\n", rows);
     }
+
+
 
     private Connection getConnection() throws SQLException, IOException {
-
         Properties props = new Properties();
         try (InputStream in = Files.newInputStream(Paths.get("database.properties"))) {
             props.load(in);
